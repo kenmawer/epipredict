a03db0aeae933e0959a8cda0849d6fb083842a64
kenmawer <kenmawer93@gmail.com>
2022-06-03 17:45:33 -0700

    Updated unsaved changes.

--- tests/testthat/test-compat-recipes.R
+++ tests/testthat/test-compat-recipes.R
@@ -6,9 +6,9 @@ triple <- function(x) {
 test_that("fun_calls works as intended",{
   expect_equal(fun_calls(add_five),"+")
   expect_equal(fun_calls(triple),c("{","*"))
-
 })
 
 test_that("fun_calls works as intended",{
-   expect_error(inline_check())
+  expect_error(inline_check(add_five),NA)
+  expect_error(inline_check(triple))
 })
