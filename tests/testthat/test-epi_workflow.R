946abd0a0c3d138213d692b066292b15a2fcf734
kenmawer <kenmawer93@gmail.com>
2022-06-14 17:26:25 -0700

    Templates for testing.

---
+++ tests/testthat/test-epi_workflow.R
@@ -0,0 +1,23 @@
+test_that("epi_workflow will behave as expected",{
+
+})
+
+test_that("is_epi_workflow works properly", {
+
+})
+
+test_that("predict.epi_workflow works properly", {
+
+})
+
+test_that("grab_forged_keys works properly", {
+
+})
+
+test_that("augment.epi_workflow works properly", {
+
+})
+
+test_that("new_epi_workflow works properly", {
+
+})
